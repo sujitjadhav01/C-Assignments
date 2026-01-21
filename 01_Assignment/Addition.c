38141a5be0e6de7ff2c7b14b760c1b49917e0617
sujitjadhav01 <sujitjadhav7023@gmail.com>
2026-01-21 11:41:08 +0530

    Addition_of_2_Numbers

---
+++ 01_Assignment/Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int N1,N2,Res;
+printf("Enter first number: ");
+scanf("%d",&N1);
+printf("Enter second number: ");
+scanf("%d",&N2);
+Res=N1+N2;
+printf("addition of the two numbers=%d",Res);
+return 0;
+}
