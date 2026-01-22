5deba9975bc569be4c26acff6d13677088e6ff22
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-01-22 21:35:53 +0530

    17_Square.c

---
+++ 01_Assignment/17_Square.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    float No=0.0, Square=0.0;
+
+    printf("Enter the number = ");
+    scanf("%f",&No);
+
+    Square= No * No;
+
+    printf("Calculate the square of given number = %0.2f",Square);
+    return 0;
+
+}
