96fcf32aec50fc0538e32c1489967829b67af7c3
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-01-24 22:20:19 +0530

    09_Month.c

---
+++ 02_Assignments/09_Month.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int Month;
+    printf("Enter the number = ");
+    scanf("%d",&Month);
+
+    if (Month ==1)
+    {
+        printf("\n January");
+    }
+    else if (Month ==2)
+    {
+        printf("\n February");
+    }
+    else if (Month ==3)
+    {
+        printf("\n March");
+    }
+    else if (Month ==4)
+    {
+        printf("\n Aprial");
+    }
+    else if (Month ==5)
+    {
+        printf("\n May");
+    }
+    else if (Month ==6)
+    {
+        printf("\n June");
+    }
+    else if (Month ==7)
+    {
+        printf("\n July");
+    }
+    else if (Month ==8)
+    {
+        printf("\n August");
+    }
+    else if (Month ==9)
+    {
+        printf("\n September");
+    }
+    else if (Month ==10)
+    {
+        printf("\n October");
+    }
+    else if (Month ==11)
+    {
+        printf("\n November");
+    }
+    else if (Month ==12)
+    {
+        printf("\n December");
+    }
+    else
+    {
+        printf("\n The number is invalide");
+    }
+
+    printf("\n\n Thanks for visiting");
+    getch();
+    return 0;
+
+}
+
