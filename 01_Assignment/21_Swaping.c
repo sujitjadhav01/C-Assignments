d5d8f8ec9f67cb86339adc2392ab6c921b794566
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-11 18:26:25 +0530

    21_Swaping.c

---
+++ 01_Assignment/21_Swaping.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+
+    int A = 0, B = 0;
+
+    printf("\n Enter value of A = ");
+    scanf("%d",&A);
+    printf("\n Enter value of B = ");
+    scanf("%d",&B);
+
+    printf("\n Befor swaping \n value of A = %d \n value of B = %d ",A,B);
+
+      getch();
+
+    printf("\n After swaping \n value of A = %d \n value of B = %d ",B,A);
+
+
+
+    printf("\n\n Thanks for visiting");
+    getch();
+    return 0;
+
+}
