fad94e39004ef188d193988ae7651d6f4bef75e6
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-11 18:46:36 +0530

    06_byte.c

---
+++ 01_Assignment/06_byte.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    printf("Size of char            :%d bytes\n",sizeof(char));
+    printf("Size of int             :%d bytes\n",sizeof(int));
+    printf("Size of float           :%d bytes\n",sizeof(float));
+    printf("Size of double          :%d bytes\n",sizeof(double));
+    printf("Size of long int        :%d bytes\n",sizeof(long int));
+    printf("Size of short int       :%d bytes\n",sizeof(short int));
+    printf("Size of long long int   :%d bytes\n",sizeof(long long int));
+
+    getch();
+    return 0;
+
+
+}
