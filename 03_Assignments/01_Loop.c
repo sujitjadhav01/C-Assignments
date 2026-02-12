8178d72147e03f17f8a927930453cfc9608367a3
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-12 20:34:11 +0530

    01_Loop.c

---
+++ 03_Assignments/01_Loop.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int No =0;
+
+    for(No = 101 ; No <= 110 ; No++)
+    {
+
+        printf("%d \n ",No);
+    }
+
+    printf("\n\n Thanks for visiting");
+
+    getch();
+    return 0;
+}
