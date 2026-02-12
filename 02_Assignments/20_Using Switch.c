33c5fbb6eec859b1f1dbcfae6a00532882898991
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-12 20:14:29 +0530

    20_Using Switch.c

---
+++ 02_Assignments/20_Using Switch.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+     int Month = 0;
+     printf("Enter number (1 TO 12) = ");
+     scanf("%d",&Month);
+
+        switch(Month)
+             {
+
+                 case 1:
+                     printf("\n January");
+                     break;
+
+                 case 2:
+                     printf("\n February");
+                     break;
+
+                 case 3:
+                     printf("\n March");
+                     break;
+
+                 case 4:
+                     printf("\n April");
+                     break;
+
+                 case 5:
+                     printf("\n May");
+                     break;
+
+                 case 6:
+                     printf("\n June");
+                     break;
+
+                 case 7:
+                     printf("\n July");
+                     break;
+
+                 case 8:
+                     printf("\n August");
+                     break;
+
+                 case 9:
+                     printf("\n September");
+                     break;
+
+                 case 10:
+                     printf("\n October");
+                     break;
+
+                 case 11:
+                     printf("\n November");
+                     break;
+
+                 case 12:
+                     printf("\n December");
+                     break;
+
+                 default :
+                     printf("\n Invalid input");
+
+             }
+
+       printf("\n\n Thanks for visiting");
+       getch();
+       return 0;
+}
