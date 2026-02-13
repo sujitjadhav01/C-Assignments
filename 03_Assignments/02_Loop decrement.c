40098b018c945555fa438ac7c29622808bd26e93
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-13 20:19:04 +0530

    02_Loop decrement.c

---
+++ 03_Assignments/02_Loop decrement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int i = 10;
+
+    for(i = 20; i >= 10; i--)
+    {
+        printf("\n %d",i);
+    }
+    getch();
+
+    printf("\n\n Thanks");
+    getch();
+    return 0;
+}
