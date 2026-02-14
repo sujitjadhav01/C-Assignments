6445f9a2a976054cbb6789aabe1041f35fec4b6e
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-14 20:57:47 +0530

    05A_Loop increment with character.c

---
+++ 03_Assignments/05A_Loop increment with character.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    char ch = '\0';
+
+    for(ch = 'A' ; ch <= 'N'; ch ++)
+    {
+
+        printf("\n %c",ch);
+    }
+
+    getch();
+
+    printf("\n\n Thanks");
+    getch();
+    return 0;
+}
