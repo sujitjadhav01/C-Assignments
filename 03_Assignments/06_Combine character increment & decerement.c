dfcf543a3fa428d2005a61087eeb52e8d7605213
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-14 20:58:37 +0530

    06_Combine character increment & decerement.c

---
+++ 03_Assignments/06_Combine character increment & decerement.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    char spt = '\0', ept = '\0';
+    printf("\n\n Enter starting point = ");
+    spt = getche();
+    printf("\n\n Enter ending point = ");
+    ept = getche();
+
+    if (spt <= ept)
+    {
+        for( ; spt <= ept; spt ++)
+        {
+            printf("\n %c",spt);
+        }
+    }
+    else
+    {
+        for( ; spt >= ept; spt --)
+        {
+            printf("\n %c",spt);
+        }
+    }
+    getch();
+
+    printf("\n\n Thanks");
+    getch();
+    return 0;
+}
