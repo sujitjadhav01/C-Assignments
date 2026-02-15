f9b14862a083af2047c1f71ef9d8eaa7bddbd7be
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-15 18:38:26 +0530

    01_Row 5 Col 4.c

---
+++ 04_Pattern printing/01_Row 5 Col 4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int i = 0, j = 0;
+
+    printf("\n\n=================================\n\n");
+
+    for(i = 1;i <= 5; i++)
+    {
+
+        for(j = 1;j <= 4; j++)
+        {
+            printf(" * ");
+        }
+         printf("\n");
+    }
+
+    printf("\n\n Thanks");
+
+    getch();
+    return 0;
+}
