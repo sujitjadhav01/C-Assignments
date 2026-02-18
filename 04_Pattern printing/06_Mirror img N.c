da3a4afb09cff325bc89227580857f14fae0e64a
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-18 20:40:09 +0530

    06_Mirror img N.c

---
+++ 04_Pattern printing/06_Mirror img N.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int i = 0, j = 0, RC = 0;
+
+    printf("\n Enter the number => ");
+    scanf("%d",&RC);
+
+    printf("\n\n==================================================================\n\n");
+
+    for(i = 1; i <= RC; i++)
+    {
+
+        for(j = 1; j <= RC; j++)
+        {
+
+            if(j == 1 || j == RC || j + i == RC + 1)
+            {
+
+                printf(" * ");
+            }
+            else
+            {
+
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n\n============================================================\n\n");
+
+    printf("\n\n Thanks");
+    getch();
+    return 0;
+}
+
