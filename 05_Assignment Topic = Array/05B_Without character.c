a023ddb3a1bad54175e98629a933520edc2fe56d
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-25 17:50:34 +0530

    05B_Without character.c

---
+++ 05_Assignment Topic = Array/05B_Without character.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+
+    char ch [5] = {'0','0','0','0','0'};
+
+    printf("\n Enter Array : %c", ch [0]);
+    printf("\n Enter Array : %c", ch [1]);
+    printf("\n Enter Array : %c", ch [2]);
+    printf("\n Enter Array : %c", ch [3]);
+    printf("\n Enter Array : %c", ch [4]);
+
+    getch();
+    return 0;
+}
