6f556aebb4bb2626f8f57fbbd95763253ad7673f
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-25 17:47:44 +0530

    01A_ User Given number.c

---
+++ 05_Assignment Topic = Array/01A_Given number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int Num [5] = {10, 20, 30, 40, 50};
+
+    printf("\n\n Enter Array %d", Num [0]);
+    printf("\n\n Enter Array %d", Num [1]);
+    printf("\n\n Enter Array %d", Num [2]);
+    printf("\n\n Enter Array %d", Num [3]);
+    printf("\n\n Enter Array %d", Num [4]);
+
+    getch();
+    return 0;
+}
