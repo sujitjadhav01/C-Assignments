5ab90beb7d2d2df4612828e8f57d2f6c25ac26d3
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-02-25 17:48:49 +0530

    03C_Two number given.c

---
+++ 05_Assignment Topic = Array/03C_Two number given.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    int Num [5] = {10, 20,};
+
+    printf("\n\n Enter Array %d", Num [0]);
+    printf("\n\n Enter Array %d", Num [1]);
+    printf("\n\n Enter Array %d", Num [2]);
+    printf("\n\n Enter Array %d", Num [3]);
+    printf("\n\n Enter Array %d", Num [4]);
+
+    getch();
+    return 0;
+}
+
