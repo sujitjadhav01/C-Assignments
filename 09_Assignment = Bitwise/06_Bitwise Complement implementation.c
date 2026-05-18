20c58aa517222ebcf12fc84eb02724dbd20d37eb
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-05-18 22:27:44 +0530

    Add files via upload

---
+++ 09_Assignment = Bitwise/06_Bitwise Complement implementation.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0, Res = 0;
+
+    printf("\n Enter a Number = ");
+    scanf("%d",&No);
+
+    printf("\n============================================\n");
+
+    Res = ~No;
+
+    printf("\n Result => ~%d = %d.", No, Res);
+
+    printf("\n\n============================================\n");
+    printf("\n Thanks for visiting ");
+
+    getch();
+    return 0;
+}
