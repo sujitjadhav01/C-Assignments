c95b68bf4ab99f8b33ece0a4cdac547b6c19fa31
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-05-20 16:13:10 +0530

    Add files via upload

---
+++ 04_Pattern printing/08_Pattern X.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+            int i = 0, j = 0, NO = 0;
+
+            printf("Enter Size for Pattern : ");
+            scanf("%d",&NO);
+
+            for(i = 1; i <= NO; i++ )
+            {
+                        for( j = 1; j <= NO; j++ )
+                        {
+                            if(i == j|| i+j == NO+1 )
+                            {
+                                    printf(" * ");
+                            }
+                            else
+                            {
+                                    printf("   ");
+                            }
+                        }
+                        printf("\n");
+            }
+
+            printf("\n Thanks for visiting ");
+
+            getch();
+            return 0;
+}
