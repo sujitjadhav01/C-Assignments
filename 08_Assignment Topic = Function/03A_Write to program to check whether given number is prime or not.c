1d795d3937e344c3eda3866c6e422d596e6faee6
Sujit Jadhav <sujitjadhav7023@gmail.com>
2026-05-20 16:44:37 +0530

    Add files via upload

---
+++ 08_Assignment Topic = Function/03A_Write to program to check whether given number is prime or not.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main() {
+    int num, i, Prime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        Prime = 0;
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                Prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (Prime == 1)
+        printf("%d is a Prime number.", num);
+    else
+        printf("%d is NOT a Prime number.", num);
+
+    return 0;
+}
